07400df1c188f78e6bf5cd011382908ed440f6fd
Berniac <bwadero@gmail.com>
2022-07-05 00:01:33 +0300

    My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def print_sorted(self):
+    """ Class that inherits from class list
+
+    Args:
+        list: class list
+
+    """
+
+    def print_sorted(self):
+        """ Method that prints the list sorted in ascending order"""
+        l_sorted = self.copy()
+        l_sorted.sort()
+        print(l_sorted)
