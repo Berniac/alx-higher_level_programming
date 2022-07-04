13a412e26f96a6fcd8042ae0c35d1bd9531e5431
BernieW <87444402+Berniac@users.noreply.github.com>
2022-07-05 00:48:26 +0300

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+Module consisting of a function that checks if an object
+is an instance of a class that inherited from a specified
+class
+"""
+
+
 def inherits_from(obj, a_class):
     """ Function that returns true/false if object is/isn't an instance
     of a class that inherited from a_class
