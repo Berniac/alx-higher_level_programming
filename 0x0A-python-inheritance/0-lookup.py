155eb91573896f78629c012abeb4cf0e9c82f2d7
BernieW <87444402+Berniac@users.noreply.github.com>
2022-07-05 00:37:04 +0300

    Update 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+Module that consists of a functiion that returns list of
+attributes and methods of obj
+
+"""
+
+
 def lookup(obj):
     """
     return the list of what is inside the obj
