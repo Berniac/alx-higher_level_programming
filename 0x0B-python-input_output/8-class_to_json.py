920e3e1cca705961e19a4819670372986117d063
Berniac <bwadero@gmail.com>
2022-07-05 22:34:05 +0300

    Class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module that contains function that returns the dictionary description
+with simple data structure for JSON serialization of an object
+"""
+
+
+def class_to_json(obj):
+    """ Function thta returns the dictionary description with simple data structure
+    for JSON serialization of an object
+    """
+
+    res = {}
+    if hasattr(obj, "__dict__"):
+        res = obj.__dict__.copy()
+    return res
