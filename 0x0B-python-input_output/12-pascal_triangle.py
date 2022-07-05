0f27af1425617e5a82918911e61e3cab5f35654e
Berniac <bwadero@gmail.com>
2022-07-05 22:36:07 +0300

    Pascal Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Module that contains function that returns a list of lists
+of integers representing the Pascal's triangle
+"""
+
+
+def pascal_triangle(n):
+    """ Function that returns Pascal's triangle
+
+    Args:
+        n: number of lines
+
+    Returns:
+        list of lists representing Pascal's triangle
+
+    """
+
+    matrix = []
+    prev = []
+
+    for i in range(n):
+        res_list = []
+        p1 = -1
+        p2 = 0
+        for j in range(len(prev) + 1):
+            if p1 == -1 or p2 == len(prev):
+                res_list += [1]
+            else:
+                res_list += [prev[p1] + prev[p2]]
+            p1 += 1
+            p2 += 1
+        matrix.append(res_list)
+        prev = res_list[:]
+
+    return matrix
