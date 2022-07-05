28cd457eab38b6e7ab2c3569aaf2b9aee05fd4f7
BernieW <87444402+Berniac@users.noreply.github.com>
2022-07-05 22:45:39 +0300

    Update 6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -15,5 +15,6 @@ def load_from_json_file(filename):
 
     """
 
-    with open(filename. 'r', encoding="utf-8") as f:
-        return json.load(f)
+    with open(filename) as f:
+        obj = f.read()
+    return(json.loads(obj))
